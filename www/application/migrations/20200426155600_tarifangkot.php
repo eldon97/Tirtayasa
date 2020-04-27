cdc98b3c783dc3c0eb81cafd6b975b2fa1202514
Stephen Jordan <98.stephenjordan@gmail.com>
2020-04-27 07:06:58 +0700

    Update column in table tracks
    column: tarifPerKm, tarifMinimal

---
+++ www/application/migrations/20200426155600_tarifangkot.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+class Migration_Tarifangkot extends CI_Migration {
+	public function up() {
+        // Add column in table tracks
+        $fields = array(
+            'tarifPerKm' => array('type' => 'INT'),
+            'tarifMinimal' => array('type' => 'INT')
+        );
+        $this->dbforge->add_column('tracks', $fields); 
+        $this->db->set('tarifMinimal', 2000);
+        $this->db->update('tracks');
+	}
+
+	public function down() {
+		// void
+	}
+}
